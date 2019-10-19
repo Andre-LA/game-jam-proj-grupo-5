afa225513ace0dcf5133d4981a1b5b0ba14ebf0d
André Luiz Alvares - 51812124 <51812124@animaedu.intranet>
2019-10-19 20:44:09 -0300

    base pronta

---
+++ unity-proj/Assets/Scripts/Sequencia.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sequencia : MonoBehaviour
+{
+    public AtivadorCena ativadorCenaAlvo;
+    public int indice = -1;
+    public string[] sequencia;
+    
+    float tempoAtual;
+    float espera = -1;
+    
+    public void Start()
+    {
+        Proximo();
+    }
+    
+    public void Proximo()
+    {
+        indice += 1;
+        if (indice < sequencia.Length)
+            ativadorCenaAlvo.estadoAnim = sequencia[indice];
+    }
+    
+    public void AbrirEscolhas(string escolhas)
+    {
+        PainelEscolhas.AbrirEscolhas(escolhas);
+    }
+    
+    public void AbrirTexto(string txtId)
+    {
+        PainelEscolhas.AbrirTexto(txtId);
+    }
+    
+    public void EsperaEProximo(float t)
+    {
+        tempoAtual = Time.time;
+        espera = t;
+    }
+    
+    void Update()
+    {
+        if (espera > 0 && Time.time - tempoAtual >= espera)
+        {
+            espera = -1;
+            Proximo();
+        }
+    }
+}
