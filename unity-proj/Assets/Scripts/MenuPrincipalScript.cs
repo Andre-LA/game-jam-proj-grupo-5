466d311d6903aa672fd48e42a6d33f1f9c8b9022
Gabriel <gabriel_msnid@hotmail.com>
2019-10-19 23:01:57 -0300

    Menu Inicial SEGUNDA versão
    Ainda não acabei

---
+++ unity-proj/Assets/Scripts/MenuPrincipalScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalScript : MonoBehaviour
+{
+    public Transform camera;
+    public Animator _animator;
+
+    public void BotaoSobre()
+    {
+        _animator.Play("Camera_Sobre");
+    }
+
+    public void Voltar()
+    {
+        _animator.Play("Camera_Sobre_Volta");
+    }
+    public void Sobe()
+    {
+        _animator.Play("CameraSobe");
+    }
+    public void SobeVolta()
+    {
+        _animator.Play("CameraSobe");
+    }
+}
