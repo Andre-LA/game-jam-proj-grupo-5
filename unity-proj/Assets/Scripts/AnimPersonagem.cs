ff61e04428f46f3a683e8b71838b1a8ea81ce478
Nicolas <nicolas.andalaft@hotmail.com>
2019-10-20 07:21:49 -0300

    Alterações nas Cenas

---
+++ unity-proj/Assets/Scripts/AnimPersonagem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AnimPersonagem : MonoBehaviour
+{
+    public DragonBones.UnityArmatureComponent dragonBonesScript;
+
+    public void _MudarVelocidade(float val)
+    {
+        dragonBonesScript._timeScale = val;
+    }
+}
